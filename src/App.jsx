472e32f2764e6a590ebb495990c522cb013c38e6
Daria Lipska <daria.nasadovich@wp.pl>
2026-05-13 16:55:23 +0200

    Wstępne przygotowanie grafu i tabelki

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,54 @@ import { useState } from 'react'
 import './index.css'
 
 function App() {
+
+
+  // To są nasze stany (pudełka na dane)
+const [wierzcholki, setWierzcholki] = useState([
+  // Przykład: { id: 0, label: 'A', x: 25, y: 30, status: 'nieodwiedzony' }
+]);
+const [krawedzie, setKrawedzie] = useState([
+  // Przykład: { od: 0, do: 1, status: 'normalna' }
+]);
+const [czas, setCzas] = useState(0);
+
+function generujGraf() { 
+    // Math.floor(Math.random() * (max - min + 1)) + min;
+    const liczbaWierzcholkow = Math.floor(Math.random() * 6) + 6;
+    const noweWierzcholki = [];
+    const noweKrawedzie = [];
+    const litery = "ABCDEFGHIJKLMNO";
+
+
+
+    // 2. Pętla tworząca wierzchołki
+   for (let i = 0; i < liczbaWierzcholkow; i++) 
+  {
+    noweWierzcholki.push({
+      id: i,
+      label: litery[i],
+      // Losujemy pozycję od 10% do 90%, żeby nie dotykały krawędzi
+      x: Math.floor(Math.random() * 80) + 10, 
+      y: Math.floor(Math.random() * 80) + 10,
+      status: 'nieodwiedzony'
+    });
+  }
+
+    for (let i = 0; i < liczbaWierzcholkow - 1; i++)
+    {
+      noweKrawedzie.push({
+        od: i,
+        do: i + 1,
+        status: 'normalna'
+      });
+    }
+
+    
+  setWierzcholki(noweWierzcholki);
+  setKrawedzie(noweKrawedzie);
+}
+
+
 return (
 <div className='contener-glowny'>
 <header>
@@ -28,11 +76,20 @@ return (
       </div>
  
       <div className="graph-container">
-
+            {wierzcholki.map((node) => (
+              <div 
+                key={node.id} 
+                className={`node ${node.status}`} 
+                style={{ position: 'absolute', left: `${node.x}%`, top: `${node.y}%` }}>
+               {node.label} {/*label to literka*/}
+             </div>
+            ))}
       </div>
 
       <div className='aktualny-stos'>
         <h2>Aktualny stos:</h2>
+         <div className='stos'>
+         </div>
       </div>
 
        </div>
@@ -41,11 +98,31 @@ return (
   <section className='contener-prawy'>
       <div className='przyciski'>
         <button>Reset</button>
-        <button>Losuj graf</button> 
+        <button onClick={generujGraf}>Losuj graf</button> 
       </div>
 
       <div className='tabela'>
         <h2>Tabela czasów:</h2>
+        <table className='tabelka'>
+          <thead>
+              <tr> {/*tr to cała linia (poziom)*/}
+                <th>Wierzchołek</th> {/*th (Table Header) to tytuł kolumny (pion).*/}
+                <th>Czas odwiedzenia</th> 
+                <th>Czas przetworzenia</th>
+              </tr>
+          </thead>
+
+          <tbody>
+              {/*td (Table Data) to zawartość (to, co wpadnie do środka tabeli później).*/}
+              {wierzcholki.map((node) => (
+               <tr key={node.id}>
+                 <td>{node.label}</td>
+                 <td>{node.d || '-'}</td> 
+                 <td>{node.f || '-'}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
 
       <div className='nastepny-krok'>
